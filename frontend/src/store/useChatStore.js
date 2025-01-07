33e117805b6bd050b381f638c0c9966805dce051
Ashishroy1724 <ashishroy78782@gmail.com>
2025-01-07 23:57:12 +0530

    Everything works fine i guess

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
+import {useAuthStore} from "./useAuthStore.js";
 
 export const useChatStore = create((set, get) => ({
   // State variables
@@ -51,6 +52,29 @@ export const useChatStore = create((set, get) => ({
     } catch (error) {
       toast.error(error.response?.data?.message || "Failed to send message.");
     }
+  },
+
+  // subscribe to messages
+  subscribeToMessages: () => {
+    const { selectedUser } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
+
+      set({
+        messages: [...get().messages, newMessage],
+      });
+    });
+  },
+
+  //unsubscribe from messages
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
   },
 
   // Set the selected user
